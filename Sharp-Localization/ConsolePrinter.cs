44c0662c95cfa02a24399bac4980ac7deaa14e16
Cornelius Wiens <cmw1334@gmail.com>
2019-06-22 18:27:17 -0500

    Added ConsolePrinter from other project. And app now prints out all cultures

---
+++ Sharp-Localization/ConsolePrinter.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Sharp_Localization
+{
+    public class ConsolePrinter
+    {
+        public int TableWidth { get; set; }
+
+        public ConsolePrinter(int tableWidth)
+        {
+            TableWidth = tableWidth;
+        }
+
+        /// <summary>
+        /// Prints single line in console at the length of the table width
+        /// </summary>
+        public void PrintLine()
+        {
+            Console.WriteLine(new string('-', TableWidth));
+        }
+
+        /// <summary>
+        /// Prints the values of the passed in string array as row values
+        /// </summary>
+        /// <param name="columns">The elements you want to print</param>
+        public void PrintRow(params string[] columns)
+        {
+            int width = (TableWidth - columns.Length) / columns.Length;
+            string row = "|";
+
+            foreach (string column in columns)
+            {
+                row += AlignCentre(column, width) + "|";
+            }
+
+            Console.WriteLine(row);
+        }
+
+        /// <summary>
+        /// Pads the passed in string value with spaces to center value in table column
+        /// </summary>
+        /// <param name="text">The text you want to align to center</param>
+        /// <param name="width">The column width</param>
+        /// <returns></returns>
+        private string AlignCentre(string text, int width)
+        {
+            text = (text.Length > width) ? text.Substring(0, width - 3) + "..." : text;
+
+            if (string.IsNullOrEmpty(text))
+            {
+                return new string(' ', width);
+            }
+            else
+            {
+                return text.PadRight(width - (width - text.Length) / 2).PadLeft(width);
+            }
+        }
+    }
+}
--- Sharp-Localization/Program.cs
+++ Sharp-Localization/Program.cs
@@ -1,12 +1,37 @@
 ﻿using System;
+using System.Globalization;
+using System.Text;
 
 namespace Sharp_Localization
 {
     class Program
     {
+        private static ConsolePrinter _printer = new ConsolePrinter(100);
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            _printer.PrintLine();
+            _printer.PrintRow(new string[] { "Code", "English Name", "Type" });
+            _printer.PrintLine();
+
+            // Print all cultures to console.
+            var allCultures = CultureInfo.GetCultures(CultureTypes.AllCultures);
+            foreach (var ci in allCultures)
+            {
+                // Get the culture type.
+                string cultureType = "";
+                if (ci.CultureTypes.HasFlag(CultureTypes.NeutralCultures))
+                    cultureType = "NeutralCulture";
+                else if (ci.CultureTypes.HasFlag(CultureTypes.SpecificCultures))
+                    cultureType = " SpecificCulture";
+
+                // Print the code, name and type to console. (Name is the code, EnglishName the desc)
+                _printer.PrintRow(new string[] {
+                    ci.Name,
+                    ci.EnglishName,
+                    cultureType
+                });
+            }
+            _printer.PrintLine();
         }
     }
 }
