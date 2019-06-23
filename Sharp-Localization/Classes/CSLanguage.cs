952c8963dd9c32892d3f803a637dc13628b90154
Cornelius Wiens <cmw1334@gmail.com>
2019-06-23 12:10:52 -0500

    Removed "Name" column from csv file. CSLanguage now reads the first column as the dictionary key.
    Also reorganized project a bit to make it easier to port to another project

--- Sharp-Localization/Classes/CSLanguage.cs
+++ Sharp-Localization/Classes/CSLanguage.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Reflection;
@@ -8,15 +9,20 @@ namespace Sharp_Localization
 {
     public class CSLanguage
     {
+        // Private Variables
+        private ConsolePrinter _printer;
+
+        // Public Variables
         public Dictionary<string, List<CSLanguageData>> CSLanguageList = new Dictionary<string, List<CSLanguageData>>();
-        
-        public CSLanguage()
+
+        public CSLanguage(int printerTableWidth = 80)
         {
+            _printer = new ConsolePrinter(printerTableWidth);
             ReadDataFromCSV();
         }
 
         /// <summary>
-        /// Reads the language data from the local CSV file.
+        /// Reads the language data from the local CSV file and parses results into local dictionary
         /// </summary>
         /// <returns>Returns FALSE if loading failed. </returns>
         private bool ReadDataFromCSV()
@@ -36,41 +42,36 @@ namespace Sharp_Localization
                 // Read data from CSV file
                 using (CsvFileReader reader = new CsvFileReader(dataFilePath))
                 {
-                    CsvRow row = new CsvRow();
-                    bool hdrRead = false;
+                    CsvRow csvRow = new CsvRow();
                     List<string> hdrRow = new List<string>();
 
-                    while (reader.ReadRow(row))
+                    while (reader.ReadRow(csvRow))
                     {
                         // Read the header row into list
-                        if (!hdrRead)
+                        if (hdrRow.Count == 0)
                         {
-                            foreach (string s in row)
+                            foreach (string s in csvRow)
                                 hdrRow.Add(s);
-
-                            hdrRead = true;
                         }
                         else
                         {
-                            List<CSLanguageData> langData = new List<CSLanguageData>();
-                            string rowName = "";
+                            List<CSLanguageData> parsedLanguageList = new List<CSLanguageData>();
+                            string nativeLanguageKey = "";
 
                             // Read row elements
-                            for (int i = 0; i < row.Count; i++)
+                            for (int i = 0; i < csvRow.Count; i++)
                             {
-                                /* Row element 0 is always the row's Name, so we read that into a string which
+                                /* Row element 0 is always the row's native language value, so we read that into a string which
                                  * will become the Dictionary's key value. */
                                 if (i == 0)
-                                    rowName = row[i];
-                                else
-                                {
-                                    /* Read row element into CSLanguageData List */
-                                    langData.Add(new CSLanguageData(hdrRow[i], row[i]));
-                                }
+                                    nativeLanguageKey = csvRow[i];
+
+                                /* Read row element into CSLanguageData List */
+                                parsedLanguageList.Add(new CSLanguageData(hdrRow[i], csvRow[i]));
                             }
 
                             // Add row's Name and values into dictionary
-                            CSLanguageList.Add(rowName, langData);
+                            CSLanguageList.Add(nativeLanguageKey, parsedLanguageList);
                         }
                     }
                 }
@@ -84,6 +85,139 @@ namespace Sharp_Localization
             }
 
             return false;
+        }
+
+        public void PrintLoadedLanguageData()
+        {
+            // Print loaded values
+            _printer.PrintLine();
+            List<string> hdr = new List<string>();
+            foreach (KeyValuePair<string, List<CSLanguageData>> item in CSLanguageList)
+            {
+                hdr.Add("Dictionary Key");
+                for (int i = 0; i < item.Value.Count; i++)
+                {
+                    hdr.Add(item.Value[i].CultureCode);
+                }
+                break;
+            }
+            _printer.PrintRow(hdr.ToArray());
+            _printer.PrintLine();
+
+            foreach (KeyValuePair<string, List<CSLanguageData>> item in CSLanguageList)
+            {
+                List<string> langValues = new List<string>();
+                langValues.Add(item.Key);
+
+                for (int i = 0; i < item.Value.Count; i++)
+                {
+                    langValues.Add(item.Value[i].Value.Trim());
+                }
+
+                _printer.PrintRow(langValues.ToArray());
+            }
+        }
+
+        /// <summary>
+        /// Prints all system cultures to console
+        /// </summary>
+        public void PrintAllCultures()
+        {
+            // Print table header
+            _printer.PrintLine();
+            _printer.PrintRow(new string[] { "Code", "English Name", "Type" });
+            _printer.PrintLine();
+
+            // Get all cultures and loop through them.
+            var allCultures = CultureInfo.GetCultures(CultureTypes.AllCultures);
+            foreach (var ci in allCultures)
+            {
+                // Get the culture type.
+                string cultureType = "";
+                if (ci.CultureTypes.HasFlag(CultureTypes.NeutralCultures))
+                    cultureType = "NeutralCulture";
+                else if (ci.CultureTypes.HasFlag(CultureTypes.SpecificCultures))
+                    cultureType = " SpecificCulture";
+
+                // Print the code, name and type to console. (Name is the code, EnglishName the desc)
+                _printer.PrintRow(new string[] {
+                    ci.Name,
+                    ci.EnglishName,
+                    cultureType
+                });
+            }
+            _printer.PrintLine();
+        }
+    }
+
+    /// <summary>
+    /// Class to hold the language culture code and translated value
+    /// </summary>
+    public class CSLanguageData
+    {
+        // https://docs.microsoft.com/en-us/bingmaps/rest-services/common-parameters-and-types/supported-culture-codes
+        public string CultureCode;
+        public string Value;
+
+        public CSLanguageData(string cultureCode, string value)
+        {
+            CultureCode = cultureCode;
+            Value = value;
+        }
+    }
+
+    public class ConsolePrinter
+    {
+        public int TableWidth { get; set; }
+
+        public ConsolePrinter(int tableWidth)
+        {
+            TableWidth = tableWidth;
+        }
+
+        /// <summary>
+        /// Prints single line in console at the length of the table width
+        /// </summary>
+        public void PrintLine()
+        {
+            Console.WriteLine(new string('-', TableWidth));
+        }
+
+        /// <summary>
+        /// Prints the values of the passed in string array as row values
+        /// </summary>
+        /// <param name="columns">The elements you want to print</param>
+        public void PrintRow(params string[] columns)
+        {
+            int width = (TableWidth - columns.Length) / columns.Length;
+            string row = "|";
+
+            foreach (string column in columns)
+            {
+                row += AlignCentre(column, width) + "|";
+            }
+
+            Console.WriteLine(row);
+        }
+
+        /// <summary>
+        /// Pads the passed in string value with spaces to center value in table column
+        /// </summary>
+        /// <param name="text">The text you want to align to center</param>
+        /// <param name="width">The column width</param>
+        /// <returns></returns>
+        private string AlignCentre(string text, int width)
+        {
+            text = (text.Length > width) ? text.Substring(0, width - 3) + "..." : text;
+
+            if (string.IsNullOrEmpty(text))
+            {
+                return new string(' ', width);
+            }
+            else
+            {
+                return text.PadRight(width - (width - text.Length) / 2).PadLeft(width);
+            }
         }
     }
 }
--- Sharp-Localization/Program.cs
+++ Sharp-Localization/Program.cs
@@ -14,66 +14,9 @@ namespace Sharp_Localization
         {
             _language = new CSLanguage();
 
-            //PrintAllCultures();
+            _language.PrintLoadedLanguageData();
 
-            // Print loaded values
-            _printer.PrintLine();
-            List<string> hdr = new List<string>();
-            foreach (KeyValuePair<string, List<CSLanguageData>> item in _language.CSLanguageList)
-            {
-                hdr.Add("Name");
-                for (int i = 0; i < item.Value.Count; i++)
-                {
-                    hdr.Add(item.Value[i].CultureCode);
-                }
-                break;
-            }
-            _printer.PrintRow(hdr.ToArray());
-            _printer.PrintLine();
-
-            foreach (KeyValuePair<string, List<CSLanguageData>> item in _language.CSLanguageList)
-            {
-                List<string> langValues = new List<string>();
-                langValues.Add(item.Key);
-
-                for (int i = 0; i < item.Value.Count; i++)
-                {
-                    langValues.Add(item.Value[i].Value.Trim());
-                }
-
-                _printer.PrintRow(langValues.ToArray());
-            }
-        }
-
-        /// <summary>
-        /// Prints all cultures to console
-        /// </summary>
-        private static void PrintAllCultures()
-        {
-            // Print table header
-            _printer.PrintLine();
-            _printer.PrintRow(new string[] { "Code", "English Name", "Type" });
-            _printer.PrintLine();
-
-            // Get all cultures and loop through them.
-            var allCultures = CultureInfo.GetCultures(CultureTypes.AllCultures);
-            foreach (var ci in allCultures)
-            {
-                // Get the culture type.
-                string cultureType = "";
-                if (ci.CultureTypes.HasFlag(CultureTypes.NeutralCultures))
-                    cultureType = "NeutralCulture";
-                else if (ci.CultureTypes.HasFlag(CultureTypes.SpecificCultures))
-                    cultureType = " SpecificCulture";
-
-                // Print the code, name and type to console. (Name is the code, EnglishName the desc)
-                _printer.PrintRow(new string[] {
-                    ci.Name,
-                    ci.EnglishName,
-                    cultureType
-                });
-            }
-            _printer.PrintLine();
+            //_language.PrintAllCultures();
         }
     }
 }
